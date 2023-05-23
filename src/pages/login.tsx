62bc997d22cd1bf2b89e62d7de3a7f612a848295
kleneway <kleneway@gmail.com>
2023-05-22 20:45:17 -0700

    Otto commit for Issue 86

---
+++ src/pages/login.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { User } from '~/types';
+
+interface Props {
+  onLogin: (user: User) => void;
+}
+
+const LoginPage: React.FC<Props> = ({ onLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Call the API endpoint to authenticate the user
+    // Handle success and error cases
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default LoginPage;
