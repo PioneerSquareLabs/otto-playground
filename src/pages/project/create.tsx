c667a08c97d73998d67557b22c7805b938188225
kleneway <kleneway@gmail.com>
2023-06-02 17:04:35 -0700

    fixes

--- src/pages/project/create.tsx
+++ src/pages/project/create.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
-import Layout from '~/components/Layout';
-import { Project } from '~/types';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Layout from "~/components/Layout";
+import { Project, User } from "~/types";
 
 interface Props {
   user: User;
@@ -9,15 +9,15 @@ interface Props {
 
 const CreateProject: React.FC<Props> = ({ user }) => {
   const router = useRouter();
-  const [projectName, setProjectName] = useState('');
-  const [projectDescription, setProjectDescription] = useState('');
-  const [language, setLanguage] = useState('');
-  const [framework, setFramework] = useState('');
-  const [library, setLibrary] = useState('');
-  const [database, setDatabase] = useState('');
-  const [hosting, setHosting] = useState('');
-  const [authFramework, setAuthFramework] = useState('');
-  const [cssFramework, setCssFramework] = useState('');
+  const [projectName, setProjectName] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [language, setLanguage] = useState("");
+  const [framework, setFramework] = useState("");
+  const [library, setLibrary] = useState("");
+  const [database, setDatabase] = useState("");
+  const [hosting, setHosting] = useState("");
+  const [authFramework, setAuthFramework] = useState("");
+  const [cssFramework, setCssFramework] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -29,7 +29,7 @@ const CreateProject: React.FC<Props> = ({ user }) => {
   return (
     <Layout>
       <div className="container mx-auto px-4">
-        <h1 className="text-2xl font-semibold mb-4">Create a New Project</h1>
+        <h1 className="mb-4 text-2xl font-semibold">Create a New Project</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="projectName" className="block text-sm font-medium">
@@ -44,7 +44,10 @@ const CreateProject: React.FC<Props> = ({ user }) => {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="projectDescription" className="block text-sm font-medium">
+            <label
+              htmlFor="projectDescription"
+              className="block text-sm font-medium"
+            >
               Project Description
             </label>
             <textarea
@@ -68,7 +71,10 @@ const CreateProject: React.FC<Props> = ({ user }) => {
             </select>
           </div>
           {/* Add more form fields for framework, library, database, hosting, authFramework, and cssFramework */}
-          <button type="submit" className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">
+          <button
+            type="submit"
+            className="mt-4 rounded bg-blue-500 px-4 py-2 text-white"
+          >
             Create Project
           </button>
         </form>
