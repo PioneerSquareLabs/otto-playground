080a2d2ace6284f91dc86ccd1541b212a8a8cb38
kleneway <kleneway@gmail.com>
2023-06-03 08:34:52 -0700

    Otto commit for Issue 313

---
+++ src/components/DataSchemaDiagram.tsx
@@ -0,0 +1,40 @@
+import React, { useMemo } from 'react';
+import { DataTable, DataColumn } from '~/types';
+
+interface Props {
+  tables: DataTable[];
+  columns: DataColumn[];
+}
+
+const DataSchemaDiagram: React.FC<Props> = ({ tables, columns }) => {
+  const columnsByTableId = useMemo(() => {
+    const groupedColumns: Record<number, DataColumn[]> = {};
+    columns.forEach((column) => {
+      if (!groupedColumns[column.table_id]) {
+        groupedColumns[column.table_id] = [];
+      }
+      groupedColumns[column.table_id].push(column);
+    });
+    return groupedColumns;
+  }, [columns]);
+
+  return (
+    <div className="data-schema-diagram">
+      {tables.map((table) => (
+        <div key={table.id} className="table">
+          <h3 className="table-name">{table.name}</h3>
+          <ul className="columns">
+            {columnsByTableId[table.id]?.map((column) => (
+              <li key={column.id} className="column">
+                <span className="column-name">{column.name}</span>
+                <span className="column-data-type">{column.data_type}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DataSchemaDiagram;
