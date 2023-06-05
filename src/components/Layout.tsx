f1e89ade696415054e037e7065f72583cc3d7c18
kleneway <kleneway@gmail.com>
2023-06-04 21:56:51 -0700

    manual fixes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,33 +1,21 @@
-import React from 'react';
-import { User } from '~/types';
+import React from "react";
+import { User } from "~/types";
 
 interface Props {
-  user: User;
   children?: React.ReactNode;
 }
 
-const Layout: React.FC<Props> = ({ user, children }) => {
+const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-gray-800 text-white py-4 px-6">
+    <div className="flex min-h-screen flex-col">
+      <header className="bg-gray-800 px-6 py-4 text-white">
         {/* header content */}
         <div className="flex items-center justify-between">
           <div className="text-xl font-semibold">AppName</div>
-          <div>
-            {user ? (
-              <span className="mr-4">{user.name}</span>
-            ) : (
-              <a href="/api/auth/signin" className="text-white">
-                Sign in
-              </a>
-            )}
-          </div>
         </div>
       </header>
-      <main className="flex-grow bg-gray-100 py-8 px-6">
-        {children}
-      </main>
-      <footer className="bg-gray-800 text-white py-4 px-6">
+      <main className="flex-grow bg-gray-100 px-6 py-8">{children}</main>
+      <footer className="bg-gray-800 px-6 py-4 text-white">
         {/* footer content */}
         <div className="text-center">
           &copy; {new Date().getFullYear()} AppName. All rights reserved.
