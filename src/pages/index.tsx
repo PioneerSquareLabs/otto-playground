e6aefe5305984534046e659fd2b517f93f315e56
kleneway <kleneway@gmail.com>
2023-06-07 14:29:54 -0700

    Otto commit for Issue 754

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from 'next/head';
 import { NextPage } from 'next';
 import { signIn } from 'next-auth/react';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const IndexPage: NextPage = () => {
-  const [email, setEmail] = useState('');
+  const router = useRouter();
 
-  const handleSignUp = async (e: React.FormEvent) => {
-    e.preventDefault();
-    signIn('email', { email });
+  const handleLoginRedirect = () => {
+    router.push('/login');
   };
 
   return (
@@ -37,7 +36,7 @@ const IndexPage: NextPage = () => {
               </p>
               <div className="mt-12">
                 <button
-                  onClick={() => signIn()}
+                  onClick={handleLoginRedirect}
                   className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
                 >
                   Sign up for free
@@ -64,34 +63,16 @@ const IndexPage: NextPage = () => {
           <div className="bg-gray-100 py-16">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <h2 className="text-3xl font-extrabold text-gray-900">Sign up</h2>
-              <form onSubmit={handleSignUp} className="mt-8 space-y-6">
-                <div className="rounded-md shadow-sm -space-y-px">
-                  <div>
-                    <label htmlFor="email" className="sr-only">
-                      Email address
-                    </label>
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                      placeholder="Email address"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </div>
-                </div>
+              <div className="mt-8 space-y-6">
                 <div>
                   <button
-                    type="submit"
+                    onClick={handleLoginRedirect}
                     className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
                     Sign up
                   </button>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
         </main>
