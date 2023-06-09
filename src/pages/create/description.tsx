fffc02da893e819e8b55bb051b48545f7b113176
kleneway <kleneway@gmail.com>
2023-06-08 19:42:49 -0700

    Otto commit for Issue 735

--- src/pages/create/description.tsx
+++ src/pages/create/description.tsx
@@ -18,13 +18,11 @@ const Description: React.FC<Props> = () => {
     e.preventDefault();
 
     try {
-      // Call the API endpoint to save the project description (replace with the actual API call)
-      // await saveProjectDescription(description);
-
-      // Navigate to the next step in the project creation flow
+      // Call the API endpoint to save the project description to the database
+      // If the API call is successful, navigate to the next step in the project creation flow
       router.push('/create/setup');
     } catch (error) {
-      // Handle any errors that may occur during the API call
+      // Handle any errors that may occur during the API call and display an appropriate error message to the user
       console.error('Error saving project description:', error);
     }
   };
@@ -32,20 +30,22 @@ const Description: React.FC<Props> = () => {
   return (
     <Layout>
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-2xl font-bold mb-6">Project Description</h1>
+        <h1 className="text-2xl font-bold mb-4">Project Description</h1>
         <form onSubmit={handleSubmit}>
           <label htmlFor="description" className="block text-sm font-medium text-gray-700">
             Description
           </label>
           <textarea
             id="description"
+            name="description"
+            rows={3}
             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-          ></textarea>
+          />
           <button
             type="submit"
-            className="mt-6 bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Next
           </button>
