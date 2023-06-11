adff557eb3ca59bd9d059483902f27411b8bffa1
kleneway <kleneway@gmail.com>
2023-06-11 16:14:30 -0700

    Otto commit for Issue 785

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -18,18 +18,22 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h1 className="text-3xl font-bold mb-6">Sign in to your account</h1>
-        <p className="text-gray-600 mb-4">
-          Sign in with your GitHub account to access the application.
-        </p>
-        <button
-          onClick={handleSubmit}
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded w-full"
-        >
-          Sign in with GitHub
-        </button>
+    <div className="bg-gray-100 w-full h-full">
+      <h1 className="text-4xl text-center text-black mt-0">Login</h1>
+      <div className="bg-white w-full h-auto rounded-lg p-6">
+        <div className="flex flex-col">
+          <label htmlFor="email" className="text-lg text-black">Email</label>
+          <input type="email" id="email" placeholder="example@email.com" className="w-full h-8 bg-gray-200 border border-solid border-gray-300 rounded-md px-2 mt-2" />
+        </div>
+        <div className="flex flex-col mt-8">
+          <div className="flex justify-between">
+            <label htmlFor="password" className="text-lg text-black">Password</label>
+            <a href="#" className="text-lg text-blue-600">Forgot password?</a>
+          </div>
+          <input type="password" id="password" placeholder="************" className="w-full h-8 bg-gray-200 border border-solid border-gray-300 rounded-md px-2 mt-2" />
+        </div>
+        <button onClick={handleSubmit} className="w-full h-9 bg-blue-600 text-white font-semibold rounded-lg mt-8">Login</button>
+        <p className="text-lg text-center mt-8">Don’t have account? <a href="#" className="text-blue-600">Create new account</a></p>
       </div>
     </div>
   );
