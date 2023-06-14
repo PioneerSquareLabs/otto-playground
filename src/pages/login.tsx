e2f84f7ab24a4c7c51a6515f76a5e33b43d003a3
kleneway <kleneway@gmail.com>
2023-06-13 17:04:48 -0700

    updates

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 const LoginPage: React.FC = () => {
   const { data: session } = useSession();
   const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
 
   React.useEffect(() => {
     if (session) {
@@ -15,10 +19,6 @@ const LoginPage: React.FC = () => {
     }
   }, [session, router]);
 
-<<<<<<< Updated upstream
-  const handleSubmit = () => {
-    signIn("github");
-=======
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
@@ -44,46 +44,78 @@ const LoginPage: React.FC = () => {
 
   const handleGoogleSignIn = () => {
     signIn("google");
->>>>>>> Stashed changes
   };
 
   return (
-    <div className="w-full h-screen bg-green-900 flex">
-      <div className="w-1/2 h-full bg-white p-4">
-        <div className="text-center text-4xl font-medium text-black uppercase">Welcome back</div>
-        <div className="text-center text-gray-500">Welcome back! Please enter your details.</div>
-        <div className="mt-10">
-          <label className="block text-black font-medium">Email</label>
-          <input className="w-full h-10 mt-2 p-2 border border-gray-300 rounded-md shadow-md" placeholder="Enter your email" />
+    <div className="flex h-screen w-full bg-green-900">
+      <div className="h-full w-1/2 bg-white p-4">
+        <div className="text-center text-4xl font-medium uppercase text-black">
+          Welcome back
+        </div>
+        <div className="text-center text-gray-500">
+          Welcome back! Please enter your details.
         </div>
         <div className="mt-10">
-          <label className="block text-black font-medium">Password</label>
-          <input type="password" className="w-full h-10 mt-2 p-2 border border-gray-300 rounded-md shadow-md" placeholder="**********" />
+          <label className="block font-medium text-black">Email</label>
+          <input
+            className="mt-2 h-10 w-full rounded-md border border-gray-300 p-2 shadow-md"
+            placeholder="Enter your email"
+            value={email}
+            onChange={handleEmailChange}
+          />
         </div>
-        <div className="flex justify-between items-center mt-4">
+        <div className="mt-10">
+          <label className="block font-medium text-black">Password</label>
+          <input
+            type="password"
+            className="mt-2 h-10 w-full rounded-md border border-gray-300 p-2 shadow-md"
+            placeholder="**********"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div className="mt-4 flex items-center justify-between">
           <div className="flex items-center">
-            <input type="checkbox" className="mr-2" />
-            <label className="text-black font-medium">Remember me</label>
+            <input
+              type="checkbox"
+              className="mr-2"
+              checked={rememberMe}
+              onChange={handleRememberMeChange}
+            />
+            <label className="font-medium text-black">Remember me</label>
           </div>
-          <a href="#" className="text-black font-medium">Forgot password</a>
+          <a
+            href="#"
+            className="font-medium text-black"
+            onClick={handleForgotPassword}
+          >
+            Forgot password
+          </a>
         </div>
         <button
           onClick={handleSubmit}
-          className="w-full h-10 mt-4 bg-red-500 rounded-md shadow-md text-white font-medium"
+          className="mt-4 h-10 w-full rounded-md bg-red-500 font-medium text-white shadow-md"
         >
           Sign in
         </button>
         <button
-          onClick={handleSubmit}
-          className="w-full h-10 mt-4 border border-gray-300 rounded-md shadow-md flex items-center justify-center"
+          onClick={handleGoogleSignIn}
+          className="mt-4 flex h-10 w-full items-center justify-center rounded-md border border-gray-300 shadow-md"
         >
-          <FontAwesomeIcon icon={faEnvelope} />
-          <span className="ml-2 text-black font-medium">Sign in with Google</span>
+          <FontAwesomeIcon icon={faGoogle} />
+          <span className="ml-2 font-medium text-black">
+            Sign in with Google
+          </span>
         </button>
-        <div className="mt-4 text-center text-sm font-medium">Don’t have an account? Sign up for free!</div>
+        <div className="mt-4 text-center text-sm font-medium">
+          Don’t have an account? Sign up for free!
+        </div>
       </div>
-      <div className="w-1/2 h-full bg-gray-300">
-        <img src="/images/f61fbb17b5c6bfbdbb6a1422915d840278950b12.jpg" className="w-full h-full object-cover" />
+      <div className="h-full w-1/2 bg-gray-300">
+        <img
+          src="/images/f61fbb17b5c6bfbdbb6a1422915d840278950b12.jpg"
+          className="h-full w-full object-cover"
+        />
       </div>
     </div>
   );
