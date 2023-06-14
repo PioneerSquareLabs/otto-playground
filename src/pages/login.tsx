e980d81388f2cae836ba582d7b871cce1b8a0850
kleneway <kleneway@gmail.com>
2023-06-14 12:26:18 -0700

    Otto commit for Issue 818

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
@@ -9,14 +9,43 @@ const LoginPage: React.FC = () => {
   const { data: session } = useSession();
   const router = useRouter();
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
   React.useEffect(() => {
     if (session) {
       router.push("/dashboard");
     }
   }, [session, router]);
 
-  const handleSubmit = () => {
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRememberMeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRememberMe(event.target.checked);
+  };
+
+  const handleSignIn = () => {
+    // Implement sign in functionality here
+    signIn("credentials", { email, password, rememberMe });
+  };
+
+  const handleSignInWithGithub = () => {
     signIn("github");
+  };
+
+  const handleForgotPassword = () => {
+    // Implement forgot password functionality here
+  };
+
+  const handleSignUp = () => {
+    // Implement sign up functionality here
   };
 
   return (
@@ -27,29 +56,29 @@ const LoginPage: React.FC = () => {
           <div className="text-base text-gray-500">Welcome back! Please enter your details.</div>
           <div className="w-full max-w-xl">
             <label className="text-base font-medium text-black">Email</label>
-            <input className="w-full h-10 border border-gray-300 rounded-md shadow-md p-2" placeholder="Enter your email" />
+            <input className="w-full h-10 border border-gray-300 rounded-md shadow-md p-2" placeholder="Enter your email" value={email} onChange={handleEmailChange} />
           </div>
           <div className="w-full max-w-xl">
             <label className="text-base font-medium text-black">Password</label>
-            <input type="password" className="w-full h-10 border border-gray-300 rounded-md shadow-md p-2" placeholder="**********" />
+            <input type="password" className="w-full h-10 border border-gray-300 rounded-md shadow-md p-2" placeholder="**********" value={password} onChange={handlePasswordChange} />
           </div>
           <div className="w-full max-w-xl flex justify-between items-center">
             <div className="flex items-center">
-              <input type="checkbox" className="w-4 h-4 border border-gray-300 rounded-md" />
+              <input type="checkbox" className="w-4 h-4 border border-gray-300 rounded-md" checked={rememberMe} onChange={handleRememberMeChange} />
               <span className="text-sm font-medium text-black ml-2">Remember me</span>
             </div>
-            <span className="text-sm font-medium text-black">Forgot password</span>
+            <span className="text-sm font-medium text-black cursor-pointer" onClick={handleForgotPassword}>Forgot password</span>
           </div>
-          <button onClick={handleSubmit} className="w-full max-w-xl h-10 bg-red-500 rounded-md shadow-md text-white font-medium">Sign in</button>
-          <button onClick={handleSubmit} className="w-full max-w-xl h-10 border border-gray-300 rounded-md shadow-md flex items-center justify-center space-x-2">
+          <button onClick={handleSignIn} className="w-full max-w-xl h-10 bg-red-500 rounded-md shadow-md text-white font-medium">Sign in</button>
+          <button onClick={handleSignInWithGithub} className="w-full max-w-xl h-10 border border-gray-300 rounded-md shadow-md flex items-center justify-center space-x-2">
             <FontAwesomeIcon icon={faGithub} />
             <span className="text-base font-medium text-black">Sign in with GitHub</span>
           </button>
-          <span className="text-sm font-medium text-center text-black">Don’t have an account? Sign up fo free!</span>
+          <span className="text-sm font-medium text-center text-black cursor-pointer" onClick={handleSignUp}>Don’t have an account? Sign up for free!</span>
         </div>
       </div>
       <div className="w-1/2 h-full bg-gray-300">
-        <img src="/images/914c4597836d1286ced052976e5c288c93299b83.jpg" alt="Splash" className="w-full h-full object-cover" />
+        <img src="/images/914c4597836d1286ced052976e5c288c93299b83.jpg" alt="User login illustration" className="w-full h-full object-cover" />
       </div>
     </div>
   );
